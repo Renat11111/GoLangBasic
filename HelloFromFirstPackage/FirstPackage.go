b58aff81aac1f89b855f56a4b053afb089cfcb85
Ренат <renat1c2016@gmail.com>
2019-12-28 16:37:05 +0400

    First commit

--- HelloFromFirstPackage/FirstPackage.go
+++ HelloFromFirstPackage/FirstPackage.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	firstPackage "github.com/Renat11111/GoLangBasic/firstPacage"
 )
 
 func main() {
 
+	var (
+		userid   = flag.String("U", "", "login_id")
+		password = flag.String("P", "", "password")
+		server   = flag.String("S", "localhost", "server_name[\\instance_name]")
+		database = flag.String("d", "", "db_name")
+	)
+
 	fmt.Println(firstPackage.HelloFromShahinozavrik())
+	fmt.Println(userid)
+	fmt.Println(password)
+	fmt.Println(server)
+	fmt.Println(database)
 
 }
